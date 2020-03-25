65aa2dfda22a8da704f77d56aa6bb3b8c6f60467
maciekwoz <33222646+dobish@users.noreply.github.com>
2020-03-25 09:51:57 +0100

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,7 +1,7 @@
 //const monk = require('monk');
 //const connectionString = 'mongodb+srv://admin:c7olk0jWG13ny88S@cluster0-r5yi9.mongodb.net/test?retryWrites=true&w=majority' || 'localhost/mevnStack';
 const mongoose = require('mongoose');
-const MONGODB_URI = 'mongodb+srv://admin:kSOtyZRA4TUuAX4S@cluster0-r5yi9.mongodb.net/Selector?retryWrites=true&w=majority'
+const MONGODB_URI = 'mongodb+srv://admin:ZWer0ipTpU9pBBZp@cluster0-r5yi9.mongodb.net/Selector?retryWrites=true&w=majority'
 mongoose.connect( MONGODB_URI ||'mongodb://localhost/mevnStack' , {useNewUrlParser: true}).then(() => console.log('connected'))
 //const db = monk(connectionString);
  let db = mongoose.connection;
@@ -9,7 +9,7 @@ mongoose.connect( MONGODB_URI ||'mongodb://localhost/mevnStack' , {useNewUrlPars
  db.on('error', console.error.bind(console, 'connection error'));
 
 db.on('connected', () => {
-    console.log('Mongoose connecte sd!')
+    console.log('Mongoose connected!')
 })
 
 
